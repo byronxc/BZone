c4f5605eb4a031052d587aa3a5c28a21f69b07fb
Byron <47398200+byronxc@users.noreply.github.com>
2022-04-26 12:13:04 -0700

    Added new style

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
+
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
@@ -14,3 +36,5 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+reveal();
